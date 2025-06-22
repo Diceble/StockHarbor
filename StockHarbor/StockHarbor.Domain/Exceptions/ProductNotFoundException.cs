740d3b25486ba1fdd7c1c06d8e28b929bdf51991
Bas van Uggelen <isabblex@gmail.com>
2025-06-22 09:45:11 +0200

    - adjust to cleaner architecture - add custom exception

---
+++ StockHarbor/StockHarbor.Domain/Exceptions/ProductNotFoundException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockHarbor.Domain.Exceptions;
+public class ProductNotFoundException : StockHarborException
+{
+    public ProductNotFoundException(string message) : base(message) { }
+    public ProductNotFoundException(int productId) : base($"Product with Id {productId} was not found.")
+    {
+    }
+}
---
+++ StockHarbor/StockHarbor.Domain/Exceptions/StockHarborException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockHarbor.Domain.Exceptions;
+public class StockHarborException(string message) : Exception(message)
+{
+}
